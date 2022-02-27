d2eec8c5198f8da6dfaa8dd7611186f3c44f4c04
Takahiro Honda <taka@192-168-1-110.tpgi.com.au>
2022-02-28 09:29:37 +1100

    feat(ci): fixing unit test

--- src/convertRgbToHex.test.ts
+++ src/convertRgbToHex.test.ts
@@ -1,4 +1,4 @@
-import { convertRgbToHex } from './convertRgbToHex';
+import { convertRgbToHex } from "./convertRgbToHex";
 
 const validInput = [
   [0, 0, 0],
@@ -6,16 +6,16 @@ const validInput = [
   [255, 0, 0],
   [0, 255, 0],
   [0, 0, 255],
-  [252, 65, 239]
+  [252, 65, 239],
 ];
 
 const validOutput = [
-  '000000',
-  'ffffff',
-  'ff0000',
-  '00ff00',
-  '0000ff',
-  'fc41ef'
+  "000000",
+  "ffffff",
+  "ff0000",
+  "00ff00",
+  "0000ff",
+  "fc41ef",
 ];
 
 const invalidInput = [
@@ -24,21 +24,23 @@ const invalidInput = [
   [0, 0, -1],
   [270, 0, 0],
   [0, 270, 0],
-  [0, 0, 270]
+  [0, 0, 270],
 ];
 
-describe('convertRgbTohex', () => {
-  it('should return correct hex for Rgb', () => {
+describe("convertRgbTohex", () => {
+  it("should return correct hex for Rgb", () => {
     validInput.map((input, key) => {
-      expect(convertRgbToHex(input[0], input[1], input[2])).toBe(validOutput[key]);
+      expect(convertRgbToHex(input[0], input[1], input[2])).toBe(
+        validOutput[key]
+      );
     });
   });
-  it('should throw an error when input is not valid', () => {
-    invalidInput.map(input => {
+  it("should throw an error when input is not valid", () => {
+    invalidInput.map((input) => {
       try {
         convertRgbToHex(input[0], input[1], input[2]);
       } catch (e) {
-        expect(e.message).toBe('Input is not valid RGB');
+        expect(e.message).toBe("Invalid RGB input");
       }
     });
   });
